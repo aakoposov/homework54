039a3dd7740220ba499a78cd01acb7521113c353
aakoposov <114219419+aakoposov@users.noreply.github.com>
2022-10-20 23:31:32 +0300

    Add files via upload

---
+++ ср5.py
@@ -0,0 +1,18 @@
+print('Выберите систему счисления: двоичная(2) или восьмиричная(8)?')
+number=input()
+if number:'2'
+print("Введите число для перевода в двоичную СС:")
+two=int(input())
+B=''
+while two>0:
+    B=str(two%2)+B
+    two=two//2
+print(B)
+if number:'8'
+print("Введите число для перевода в восьмиричную СС:")
+eight = int(input())
+V = ''
+while eight>0:
+     V= str(eight % 8)+V
+     eight= eight //8
+print(V)
